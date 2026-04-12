612c94d40f2dac72c09547d432e6bab75e86d362
Shiva Choudhry <chaudharyshiva2008@gmail.com>
2026-04-12 13:17:47 +0530

    successfult_printed_vectors

--- src/service/vector.service.js
+++ src/service/vector.service.js
@@ -15,7 +15,15 @@ async function createMemory({vectors,metadata,messageId}) {
     }])
 }
 
-async function queryMemory(queryVector,limit=5){
-    
+async function queryMemory({queryVector,limit=5,metadata}){
+    const data=await cohortChatGptIndex.query({
+        vector:queryVector,
+        topK:limit,
+        filter:metadata ? {metadata}:undefined,
+        includeMetadata:true
+    })
+
+    return data.matches
 }
 
+module.exports={createMemory,queryMemory};
